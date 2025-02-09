6d3ab644ec790ce9d6ce851f95e9630f4022fff6
shaishabcoding <shaishabchandrashil@gmail.com>
2025-02-09 17:04:13 +0600

    enhance user validation schemas to enforce stronger password requirements and add optional update fields

--- src/app/modules/user/User.validation.ts
+++ src/app/modules/user/User.validation.ts
@@ -4,18 +4,30 @@ const registerSchema = z.object({
   body: z.object({
     name: z.string(),
     email: z.string().email(),
-    password: z.string().min(8),
-    /** Todo */
-    // .regex(/[a-z]/, 'must contain at least one lowercase letter')
-    // .regex(/[A-Z]/, 'must contain at least one uppercase letter')
-    // .regex(/[0-9]/, 'must contain at least one number')
-    // .regex(/[^a-zA-Z0-9]/, 'must contain at least one special character'),
+    password: z
+      .string()
+      .min(8)
+      .regex(/[a-z]/, 'must contain at least one lowercase letter')
+      .regex(/[A-Z]/, 'must contain at least one uppercase letter')
+      .regex(/[0-9]/, 'must contain at least one number')
+      .regex(/[^a-zA-Z0-9]/, 'must contain at least one special character'),
     avatar: z
       .string()
       .regex(/^\/[^/].*$/, 'must be a valid path starting with /'),
   }),
 });
 
+const updateSchema = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    avatar: z
+      .string()
+      .regex(/^\/[^/].*$/, 'must be a valid path starting with /')
+      .optional(),
+  }),
+});
+
 export const UserValidation = {
   registerSchema,
+  updateSchema,
 };
