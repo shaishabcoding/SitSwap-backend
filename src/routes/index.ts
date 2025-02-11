ac868422e371d2eefb3bda76d68fc9b42803ad7f
shaishabcoding <shaishabchandrashil@gmail.com>
2025-02-10 16:24:56 -0800

    refactor routes to include admin routes and improve variable naming

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,25 @@
-import express, { Router } from 'express';
+import { Router } from 'express';
 import { AuthRoutes } from '../app/modules/auth/Auth.route';
+import { AdminRoutes } from '../app/modules/admin/Admin.route';
 
-const router = express.Router();
+const router = Router();
 
-const apiRoutes: { path: string; route: Router }[] = [
+const apis: { path: string; route: Router }[] = [
+  // * AUTH routes => start
   {
     path: '/',
     route: AuthRoutes,
   },
+  // * AUTH routes => end
+  //>>>>>>>>>>>>>>>>>>>>>
+  // * ADMIN routes => start
+  {
+    path: '/admin',
+    route: AdminRoutes,
+  },
+  // * ADMIN routes => end
 ];
 
-apiRoutes.forEach(route => router.use(route.path, route.route));
+apis.forEach(({ path, route }) => router.use(path, route));
 
 export default router;
