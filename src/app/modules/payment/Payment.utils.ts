5d9b520781e62ded85c43c3808f0e1e4825ca0e4
shaishabcoding <shaishabchandrashil@gmail.com>
2025-02-11 15:02:31 +0600

    add PayPal SDK integration with client setup and order/payment controllers

--- src/app/modules/payment/Payment.utils.ts
+++ src/app/modules/payment/Payment.utils.ts
@@ -1,5 +1,12 @@
 import bt from 'braintree';
 import config from '../../../config';
+import {
+  Client,
+  Environment,
+  LogLevel,
+  OrdersController,
+  PaymentsController,
+} from '@paypal/paypal-server-sdk';
 
 export const braintree = new bt.BraintreeGateway({
   environment:
@@ -10,3 +17,25 @@ export const braintree = new bt.BraintreeGateway({
   publicKey: config.payment.braintree.publicKey as string,
   privateKey: config.payment.braintree.privateKey as string,
 });
+
+export const paypalClient = new Client({
+  clientCredentialsAuthCredentials: {
+    oAuthClientId: config.payment.paypal.client as string,
+    oAuthClientSecret: config.payment.paypal.secret as string,
+  },
+  timeout: 0,
+  environment:
+    process.env.NODE_ENV === 'production'
+      ? Environment.Production
+      : Environment.Sandbox,
+  logging: {
+    logLevel: LogLevel.Info,
+    logRequest: { logBody: true },
+    logResponse: { logHeaders: true },
+  },
+});
+
+export const paypal = {
+  order: new OrdersController(paypalClient),
+  payment: new PaymentsController(paypalClient),
+};
