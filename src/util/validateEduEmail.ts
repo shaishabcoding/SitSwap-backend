67330ee31d0a087d02602ec5eb09df8ee41820f9
shaishabcoding <shaishabchandrashil@gmail.com>
2025-02-13 17:24:55 +0600

    refactor email validation function to use axios request method and updated API endpoint

--- src/util/validateEduEmail.ts
+++ src/util/validateEduEmail.ts
@@ -2,13 +2,17 @@ import axios from 'axios';
 import config from '../config';
 
 async function validateEduEmail(email: string): Promise<boolean> {
-  const response = await axios.post(
-    'https://api.apyhub.com/validate/academic-email',
-    { email: email },
-    { headers: { 'apy-token': config.apyhub_api as string } },
-  );
+  const { data } = await axios.request({
+    method: 'POST',
+    url: 'https://api.apyhub.com//validate/email/academic',
+    headers: {
+      'Content-Type': 'application/json',
+      'apy-token': config.apyhub_api,
+    },
+    data: { email },
+  });
 
-  return response.data.success;
+  return data.success;
 }
 
 export default validateEduEmail;
