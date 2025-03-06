d7c4d194fee26245db6b8497e47d63fc4fd6f703
shaishabcoding <shaishabchandrashil@gmail.com>
2025-03-06 17:48:33 +0600

    fixed route

--- src/app/modules/product/Product.route.ts
+++ src/app/modules/product/Product.route.ts
@@ -3,6 +3,10 @@ import { ProductController } from './Product.controller';
 import imageUploader from '../../middlewares/imageUploader';
 import purifyRequest from '../../middlewares/purifyRequest';
 import { ProductValidation } from './Product.validation';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { ReviewController } from '../review/Review.controller';
+import { ReviewValidation } from '../review/Review.validation';
 
 const adminRoutes = Router();
 
@@ -31,6 +35,19 @@ adminRoutes.delete('/:productId/delete', ProductController.delete);
 const customerRoutes = Router();
 
 customerRoutes.get('/', ProductController.list);
+
+customerRoutes.patch(
+  '/:productName/reviews/store',
+  auth('admin', 'user'),
+  validateRequest(ReviewValidation.store),
+  ReviewController.store,
+);
+
+customerRoutes.get(
+  '/:name/reviews',
+  ({ body, params }, _, next) => ((body.product = params.name), next()),
+  ReviewController.list,
+);
 
 export const ProductRoutes = {
   adminRoutes,
